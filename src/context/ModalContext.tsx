568870bd0323522598929b6bc5f5dad7df0244b3
Sehee Jang <seheejang94@gmail.com>
2025-04-28 15:43:10 +0900

    refactor: ModalContext 타입스크립트로 변환

---
+++ src/context/ModalContext.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+type ModalType = "login" | "availability" | "reservationDetail" | null;
+
+interface ModalContextType {
+  modalType: ModalType;
+  modalProps: any;
+  showModal: (type: ModalType, props?: any) => void;
+  closeModal: () => void;
+}
+
+const ModalContext = createContext<ModalContextType>({
+  modalType: null,
+  modalProps: null,
+  showModal: () => {},
+  closeModal: () => {},
+});
+
+export const useModal = () => useContext(ModalContext);
+
+interface ModalProviderProps {
+  children: ReactNode;
+}
+
+export const ModalProvider = ({ children }: ModalProviderProps) => {
+  const [modalType, setModalType] = useState<ModalType>(null); // 'login' | 'availability' | 'reservationDetail'
+  const [modalProps, setModalProps] = useState<any>(null);
+
+  const showModal = (type: ModalType, props: any = null) => {
+    setModalType(type);
+    setModalProps(props);
+  };
+
+  const closeModal = () => {
+    setModalType(null);
+    setModalProps(null);
+  };
+
+  return (
+    <ModalContext.Provider
+      value={{ modalType, modalProps, showModal, closeModal }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
+};
