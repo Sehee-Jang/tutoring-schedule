e7985061367309ca86fa537281146180ffc0950f
Sehee Jang <seheejang94@gmail.com>
2025-05-03 07:52:37 +0900

    add: 튜터 페이지 레이아웃 생성

--- src/components/tutor/Sidebar.tsx
+++ src/components/tutor/Sidebar.tsx
@@ -1,1 +1,9 @@
 // 좌측 메뉴 & 알림
+
+import React from "react";
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
--- src/pages/tutor/TutorLayout.tsx
+++ src/pages/tutor/TutorLayout.tsx
@@ -1,1 +1,20 @@
-// 좌측 사드 + 메인 구조 레이아웃
+// 좌측 사이드 + 메인 구조 레이아웃
+import { ReactNode } from "react";
+import Sidebar from "../../components/tutor/Sidebar";
+
+interface TutorLayoutProps {
+  children: ReactNode;
+}
+
+const TutorLayout = ({ children }: TutorLayoutProps) => {
+  return (
+    <div className='flex h-screen'>
+      <aside className='w-[250px] border-r border-gray-200 bg-white'>
+        <Sidebar />
+      </aside>
+      <main className='flex-1 overflow-y-auto bg-gray-50 p-6'>{children}</main>
+    </div>
+  );
+};
+
+export default TutorLayout;
