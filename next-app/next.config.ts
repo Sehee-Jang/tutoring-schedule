57dd55d274e74ed60c95bb050fccdc2d6892a694
Sehee Jang <seheejang94@gmail.com>
2025-05-09 16:25:35 +0900

    fix: 빌드 오류

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -4,24 +4,24 @@ import type { Configuration } from "webpack";
 const nextConfig: NextConfig = {
   webpack: (config: Configuration, { isServer }) => {
     if (!isServer) {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          ...config.resolve?.fallback,
-          fs: false,
-          stream: false,
-          net: false,
-          tls: false,
-          zlib: false,
-        },
-        alias: {
-          ...config.resolve?.alias,
-          "gzip-size": false, // gzip-size 무시
-        },
-      };
+      if (config.resolve) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false, // 파일 시스템 모듈을 완전히 무시
+          stream: false, // 스트림 모듈 무시
+          net: false, // 네트워크 모듈 무시
+          tls: false, // TLS 모듈 무시
+          zlib: false, // 압축 모듈 무시
+          "gzip-size": false, // gzip-size 모듈 무시
+        };
+      }
     }
-
     return config;
+  },
+  experimental: {
+    outputFileTracingIgnores: [
+      "**/node_modules/next/dist/compiled/gzip-size/**",
+    ],
   },
 };
 
