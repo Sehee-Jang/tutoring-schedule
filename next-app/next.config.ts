ce250b5c9b05a0540f6ddca74515e923f4b160bd
Sehee Jang <seheejang94@gmail.com>
2025-05-09 13:00:17 +0900

    fix: 빌드 오류

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -4,20 +4,21 @@ import type { Configuration } from "webpack";
 const nextConfig: NextConfig = {
   webpack: (config: Configuration, { isServer }) => {
     if (!isServer) {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          ...config.resolve?.fallback,
-          fs: false,
-          net: false,
-          tls: false,
-          zlib: false,
-          stream: require.resolve("stream-browserify"),
-        },
-        alias: {
-          ...config.resolve?.alias,
-          stream: require.resolve("stream-browserify"),
-        },
+      // config.resolve가 존재하는지 확인
+      config.resolve = config.resolve || { fallback: {}, alias: {} };
+
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        zlib: false,
+        stream: false, // stream을 false로 설정
+      };
+
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        stream: false, // stream alias도 false로 설정
       };
     }
 
