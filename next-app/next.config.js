fedc56a13e073c4dd0096b43be0f8fed11f6a463
Sehee Jang <seheejang94@gmail.com>
2025-05-09 17:04:34 +0900

    downgrade: node.js

---
+++ next-app/next.config.js
@@ -0,0 +1,71 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      if (config.resolve) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+          stream: false,
+          net: false,
+          tls: false,
+          zlib: false,
+        };
+
+        config.resolve.alias = {
+          ...(config.resolve.alias || {}),
+          "gzip-size": false,
+          "next/dist/compiled/gzip-size": false,
+        };
+      }
+    }
+
+    return config;
+  },
+  experimental: {
+    outputFileTracingIgnores: [
+      "**/node_modules/next/dist/compiled/gzip-size/**",
+      "**/node_modules/gzip-size/**",
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
+// next.config.ts
+// import type { NextConfig } from "next";
+// import type { Configuration } from "webpack";
+
+// const nextConfig: NextConfig = {
+//   webpack: (config: Configuration, { isServer }) => {
+//     if (!isServer) {
+//       if (config.resolve) {
+//         config.resolve.fallback = {
+//           ...config.resolve.fallback,
+//           fs: false,
+//           stream: false,
+//           net: false,
+//           tls: false,
+//           zlib: false,
+//         };
+
+//         config.resolve.alias = {
+//           ...(config.resolve.alias || {}),
+//           "gzip-size": false, // gzip-size 모듈 완전히 무시
+//           "next/dist/compiled/gzip-size": false, // Next.js 내부 gzip-size 무시
+//         };
+//       }
+//     }
+
+//     return config;
+//   },
+//   experimental: {
+//     outputFileTracingIgnores: [
+//       "**/node_modules/next/dist/compiled/gzip-size/**",
+//       "**/node_modules/gzip-size/**",
+//     ],
+//   },
+// };
+
+// export default nextConfig;
