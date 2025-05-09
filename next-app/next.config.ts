bc512ba53a072bbf77d386099ad97270bd318a34
Sehee Jang <seheejang94@gmail.com>
2025-05-09 14:20:52 +0900

    Remove gzip-size and stream-browserify and update next.config.ts

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -3,22 +3,15 @@ import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
   webpack: (config: Configuration, { isServer }) => {
+    // 서버 환경이 아닌 경우 Node.js 모듈 무효화
     if (!isServer) {
-      // config.resolve가 존재하는지 확인
-      config.resolve = config.resolve || { fallback: {}, alias: {} };
-
+      config.resolve = config.resolve || {};
       config.resolve.fallback = {
-        ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
         zlib: false,
-        stream: false, // stream을 false로 설정
-      };
-
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        stream: false, // stream alias도 false로 설정
+        stream: false, // 여기서 stream을 false로 설정
       };
     }
 
