c2950bdbc6a3fd120acbf7bc946c191eded160fa
Sehee Jang <seheejang94@gmail.com>
2025-05-09 15:03:13 +0900

    fix: 빌드오류해결중

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -3,15 +3,17 @@ import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
   webpack: (config: Configuration, { isServer }) => {
-    // 서버 환경이 아닌 경우 Node.js 모듈 무효화
     if (!isServer) {
-      config.resolve = config.resolve || {};
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        tls: false,
-        zlib: false,
-        stream: false, // 여기서 stream을 false로 설정
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve?.fallback,
+          fs: false, // 파일 시스템 모듈을 완전히 무시
+          stream: false,
+          net: false,
+          tls: false,
+          zlib: false,
+        },
       };
     }
 
